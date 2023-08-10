2d2597645bc96e46354276eaab1a70ecd67ebf6f
cody <cody@simtable.com>
2023-08-10 16:59:04 -0600

    monocular depth

--- featureMatcher.py
+++ featureMatcher.py
@@ -1,14 +1,14 @@
 import skimage
 import matplotlib
 from kornia.feature import LoFTR
+import kornia.feature as KF
 import torch as torch
 import cv2
 import numpy as np
 
 
-def matchFeaturesLOFTR(img1, img2, pretrainedName='outdoor'):
-    matcher = LoFTR(pretrained=pretrainedName)
-    matcher = matcher.eval().cpu()
+def _matchFeatures(img1, img2, matcher):
+
     # convert to grayscale if the input image is a color image
     if len(img1.shape) > 2:
         # img1 = skimage.color.rgb2gray(img1)
@@ -19,7 +19,7 @@ def matchFeaturesLOFTR(img1, img2, pretrainedName='outdoor'):
         img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
         print('img2 to grayscale')
     # resize images while maintaining aspect ratio
-    newDim= (360,640)
+    newDim= (640,640)
     img1Shape = img1.shape
     img2Shape = img2.shape
 
@@ -50,7 +50,15 @@ def matchFeaturesLOFTR(img1, img2, pretrainedName='outdoor'):
 
     return mkpts0.T, mkpts1.T, mconf
 
+def matchFeaturesLOFTR(img1, img2, pretrainedName='outdoor'):
+    matcher = LoFTR(pretrained=pretrainedName)
+    matcher = matcher.eval().cpu()
+    return _matchFeatures(img1, img2, matcher)
 
+def matchFeaturesHardnet(img1, img2):
+    matcher = KF.LocalFeatureMatcher(KF.GFTTAffNetHardNet(3000), KF.DescriptorMatcher('smnn', 0.9))
+    matcher = matcher.eval().cpu()
+    return _matchFeatures(img1, img2, matcher)
 
 def drawMatches(img1, img2, keypoints1, keypoints2, confidences):
     colorMap = matplotlib.cm.get_cmap('Spectral')
@@ -61,6 +69,22 @@ def drawMatches(img1, img2, keypoints1, keypoints2, confidences):
         k1 = keypoints1[i]
         k2a = keypoints2[i]
         k2b = (k2a[0] + img1.shape[1], k2a[1])
+        color = colorMap(confidences[i])
+        colorb = (color[0]*255, color[1]*255, color[2]*255)
+        cv2.line(newImage, (int(k1[0]), int(k1[1])), (int(k2b[0]), int(k2b[1])), colorb, 1)
+        cv2.circle(newImage, (int(k1[0]), int(k1[1])), 5, colorb, 1)
+        cv2.circle(newImage, (int(k2b[0]), int(k2b[1])), 5, colorb, 1)
+    return newImage
+
+def drawMatchesVertical(img1, img2, keypoints1, keypoints2, confidences):
+    colorMap = matplotlib.cm.get_cmap('Spectral')
+    newImage = np.vstack((img1, img2))
+    if len(newImage.shape) == 2:
+        newImage = skimage.color.gray2rgb(newImage)
+    for i in range(len(keypoints1)):
+        k1 = keypoints1[i]
+        k2a = keypoints2[i]
+        k2b = (k2a[0], k2a[1] + img1.shape[0])
         color = colorMap(confidences[i])
         colorb = (color[0]*255, color[1]*255, color[2]*255)
         cv2.line(newImage, (int(k1[0]), int(k1[1])), (int(k2b[0]), int(k2b[1])), colorb, 1)
